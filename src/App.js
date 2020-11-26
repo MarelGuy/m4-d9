a18f7c44cd6a49dfef1d90e2da322142d3403b9a
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2020-11-26 15:57:51 +0100

    fixing warnings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 
-import { Modal } from "react-bootstrap";
+
 import MovieList from "./components/MovieList";
-import ModalMovie from "./components/ModalMovie";
 import Footer from "./components/Footer";
 import NavBar from './components/NavBar';
-import { Link, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import SinlgeMoviePage from "./components/SinlgeMoviePage";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,19 +13,20 @@ function App() {
   return (
     <div className="App">
 
-       <NavBar />
+      <NavBar />
       <Switch>
         <Route exact path="/">
-      <MovieList />
-      {/* <ModalMovie />   */}
+          <MovieList />
+          {/* <ModalMovie />   */}
         </Route>
         <Route exact path="/movie/:id">
           <SinlgeMoviePage />
-          </Route>
+
+        </Route>
 
       </Switch>
-      
-         <Footer />
+
+      <Footer />
 
     </div>
   );
---
+++ src/components/RegPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RegPage extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
