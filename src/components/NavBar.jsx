7ed584ccfac8592b40046024a25ef563179156b3
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2020-11-26 15:55:31 +0100

    template

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Navbar, Nav, NavDropdown, Form } from 'react-bootstrap'
+import logo from '../imgs/LOGO.png'
+import '../styles/NavBar.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+const NavBar = () => {
+
+    return (
+        <>
+            <Navbar expand="lg" className='nav'>
+                <Navbar.Brand href="#home"><img className='logo' src={logo} alt='netflix' /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home" className='nav'>Home</Nav.Link>
+                        <Nav.Link href="#link" className='nav'>Tv Shows</Nav.Link>
+                        <Nav.Link href="#link" className='nav'>Movies</Nav.Link>
+                        <Nav.Link href="#link" className='nav'>Recently Added</Nav.Link>
+                        <Nav.Link href="#link" className='nav'>My List</Nav.Link>
+                    </Nav>
+                    <Form inline>
+
+                        <FontAwesomeIcon icon={faSearch} />
+                        <Nav.Link href='#link' className='nav'>KIDS</Nav.Link>
+                        <NavDropdown title='User' menuAlign="right" id="dropdown-menu-align-right" className='nav'
+                            style={{ color: 'gray !important' }}>
+                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Register</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.3">Ara Ara</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href='#link'></Nav.Link>
+                        <Nav.Link href='#link'></Nav.Link>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    );
+}
+
+export default NavBar;
